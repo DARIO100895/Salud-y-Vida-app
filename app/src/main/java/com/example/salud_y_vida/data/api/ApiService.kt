bd1f342d69b535c380a9973a27eeaa6d95366a2e
Ruben Benites Morales <i202405981@cibertec.edu.pe>
2025-10-24 01:26:09 -0500

    Recuperando archivos del commit 5a87064

--- app/src/main/java/com/example/salud_y_vida/data/api/ApiService.kt
+++ app/src/main/java/com/example/salud_y_vida/data/api/ApiService.kt
@@ -15,26 +15,26 @@ interface ApiService {
     //Paciente
     @GET("api/v1/pacientes")
     suspend fun getPaciente() : Response<List<Paciente>>
-    @GET("api/v1/pacientes/{id}")
+    @GET("api/pacientes/{id}")
     suspend fun getPaciente(@Path("id") id: Int): Response<Paciente>
-    @POST("api/v1/pacientes")
+    @POST("api/pacientes")
     suspend fun  createPaciente(@Body paciente: Paciente) : Response<Paciente>
-    @PUT("api/v1/pacientes/{id}")
+    @PUT("api/pacientes/{id}")
     suspend fun updatePaciente (@Path("id") id : Int, @Body paciente: Paciente) : Response<Paciente>
-    @DELETE("api/v1/pacientes/{id}")
+    @DELETE("api/pacientes/{id}")
     suspend fun deletePaciente(@Path("id") id: Int) : Response<Void>
 
     //CitaMedica
 
     @GET("api/v1/cita")
     suspend fun getCita() : Response<List<Cita>>
-    @GET("api/v1/cita/{id}")
+    @GET("api/citas/{id}")
     suspend fun getCita(@Path("id")id: Int) : Response<Cita>
-    @POST("api/v1/cita")
+    @POST("api/citas")
     suspend fun createCita (@Body cita: Cita) : Response<Cita>
-    @PUT("api/v1/cita/{id}")
+    @PUT("api/citas/{id}")
     suspend fun updateCita (@Path("id")id: Int, @Body cita: Cita) : Response<Cita>
-    @DELETE("api/v1/cita/{id}")
+    @DELETE("api/cita/{id}")
     suspend fun deleteCita(@Path("id")id: Int) : Response<Void>
 
     //Medicos
